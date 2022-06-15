d187ec14a990297541d3bbc8fe7e3338207c1480
Reza Yusuf Maulana <45605134+rezaa98@users.noreply.github.com>
2022-06-15 16:18:26 +0700

    Nambahin Java Sesi 3

---
+++ Java/Sesi3/Latihan5_DoWhile.java
@@ -0,0 +1,14 @@
+public class Latihan5_DoWhile {
+    public static void main(String[] args) {
+
+        System.out.println("Hasil Output : ");
+
+        int i = 0;
+        //perulangan do
+        do {
+                System.out.println("Perulangan ke-"+ i +" kali");
+                i++;
+        }
+        while ( i<=10); //ini buat kondisi yang buat di ulang
+        }
+    }
