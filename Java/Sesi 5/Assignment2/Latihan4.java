900be214f0af71c05069d764340add802769a656
Reza Yusuf Maulana <45605134+rezaa98@users.noreply.github.com>
2022-06-16 19:16:40 +0700

    Update Assignment Tahun Kabisat
    nambahin fitur di tahun kabisat

--- Java/Sesi 5/Assignment2/Latihan4.java
+++ Java/Sesi 5/Assignment2/Latihan4.java
@@ -1,5 +1,27 @@
 package Assignment2;
 
+import java.util.Scanner;
+
 public class Latihan4 {
-    
+  public static void main(String[] args) {
+    int tahun;
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("==================================");
+    System.out.println("======PERAMAL TAHUN KABISAT=======");
+    System.out.println("==================================");
+
+    System.out.print("Masukkan tahun : ");
+    tahun = scan.nextInt();
+
+    if (tahun % 400 == 0) {
+      System.out.print(tahun + " Merupakan Tahun Kabisat\n");
+    } else if (tahun % 100 == 0) {
+      System.out.println(tahun + " Bukan Tahun Kabisat\n");
+    } else if (tahun % 4 == 0) {
+      System.out.print(tahun + " Merupakan Tahun Kabisat\n");
+    } else {
+      System.out.print(tahun + " Bukan Tahun Kabisat\n");
+    }
+  }
 }
