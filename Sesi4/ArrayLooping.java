179c2add38a05745170aacfab61ac52206a0b724
Reza Yusuf Maulana <45605134+rezaa98@users.noreply.github.com>
2022-06-16 19:04:41 +0700

    Delete Sesi4 directory

--- Sesi4/ArrayLooping.java
+++
@@ -1,9 +0,0 @@
-public class ArrayLooping {
-    public static void main(String[] args) {
-        String[] bahasa ={"Reactjs", "Reactnative", "golang", "PHP"};
-
-        for(int i=0; i<bahasa.length; i++){
-            System.out.println("Indeks ke-"+i+": "+ bahasa[i]);
-        }
-    }
-}
--- Sesi4/readme.txt
+++
@@ -1,5 +0,0 @@
-Nama : Reza Yusuf Maulana
-
-Pre order apa yang kita input, jadi ari kiri ke kanan
-inorder yang merapikan susunan kita, jadi telusur kekiri, cetak, cari ke kanan
-post order itu yang diambiladalah 2 terdepan, telusur ke kiri, cari kekanan, baru cetak
