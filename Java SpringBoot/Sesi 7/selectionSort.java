be4dddfd2b6f026c4ca02f63bee00d242a59ea50
rezaa98 <reza.yusuf98@gmail.com>
2022-06-17 14:17:05 +0700

    Update selectionSort.java
    Nambah sesi 7 Selection Sort

--- Java SpringBoot/Sesi 7/selectionSort.java
+++ Java SpringBoot/Sesi 7/selectionSort.java
@@ -1,36 +1,38 @@
 import java.util.Scanner;
 
-public class selectionShort {
+public class selectionSort {
     public static void main(String[] args) {
         //bikin objek scanner, mesti lupa syntax scanner heran dah
         Scanner scan = new Scanner (System.in);
 
         //input jumlah datanya
-        System.out.println("Masukkan jumlah data : ");
+        System.out.print("Masukkan jumlah data : ");
         int jmlh_data = scan.nextInt();
 
         //input nilai tiap data
         int [] data = new int[jmlh_data]; //iniarray untuk tiap data ya ges yak
-        System.out.println(x);
+        System.out.println();
         for (int x = 0; x < jmlh_data; x++){
             System.out.println("Input nilai Data ke -"+(x+1)+" : ");
             data[x] = scan.nextInt();
         }
 
         //Tampilkan data sebelum di urutin atau di sorting yagesyak
-        System.out.println(x);
+        System.out.println();
         System.out.println("Data Sebelum di Sorting : ");
         for( int x = 0; x < jmlh_data; x++){
         System.out.println(data[x]+" ");
         }
+    
         //proses selection sort
         System.out.println("\n\nProses Selection Sort");
         for (int x = 0; x < jmlh_data; x++){
             System.out.println("Iterasi ke-"+(x+1)+" : ");
-            for(int y = 0; y < jmlh_data; y++)
-            System.out.println(data[y]+" ");
-
-            System.out.println("Apakah Data"+data[x]+" sudah benar pada urutannya????");
+            for(int y = 0; y < jmlh_data; y++){
+            System.out.print(data[y]+" ");
+            }
+            System.out.println("");;
+            System.out.println("Apakah Data "+data[x]+" sudah benar pada urutannya????");
 
             boolean tukar = false;
             int index = 0;
@@ -48,15 +50,16 @@ public class selectionShort {
             if(tukar == true)
             {
                 // Pertukaran Data
-                pesan = " Data "+data[x]+ "ditukar dengan Data "+data[index];
+                pesan = " Data "+data[x]+ " ditukar dengan Data "+data[index];
                 int temp = data [x];
                 data [x] = data [index];
                 data [index] = temp;
             }
 
-            for(int y = 0; y < jmlh_data; y++)
-                System.out.println(data[y]+" ");
-
+            for(int y = 0; y < jmlh_data; y++){
+                System.out.print(data[y]+" ");
+            }
+            System.out.println("");
             System.out.println(pesan+"\n");
         }
     }
