d1e936ae6fd4e8762a0a869ac22243e9e0d86437
rezaa98 <reza.yusuf98@gmail.com>
2022-06-28 08:00:56 +0700

    update lagi

---
+++ Java SpringBoot/Sesi 21/HelloJavaSpring_ver2/src/test/java/com/example/demo/HelloJavaSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
